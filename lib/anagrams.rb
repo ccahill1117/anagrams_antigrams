472424af3a3f4cbaef5ebf859addc107d5219c6c
Christopher Cahill <cthomascahill@gmail.com>
2018-11-16 08:53:21 -0800

    first test passing

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,1 +1,17 @@
 require ('pry')
+
+class Grams
+  attr_accessor(:first_string, :second_string)
+
+  def initialize(first_string, second_string)
+    @first_string = first_string
+    @second_string = second_string
+
+  end
+
+  def anagram(first_string, second_string)
+    @first_string.split('')
+    @second_string.split('')
+  end
+
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,3 +1,11 @@
 require ('rspec')
-require ('prime_sifting')
+require ('anagrams')
 require ('pry')
+
+describe('Grams#initialize') do
+  it('takes two user inputs') do
+    grams = Grams.new("hey","hi")
+    expect(grams.first_string).to eq("hey")
+    expect(grams.second_string).to eq("hi")
+  end
+end
